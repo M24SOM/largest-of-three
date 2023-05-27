8aa9385b8db693f05f8aa7255b7da44c8d7aeb12
Mohamed Mahad Farah <mmfarah@proton.me>
2023-05-27 11:47:55 +0300

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let num1 = 10;
+let num2 = 20;
+let num3 = 15;
+
+if (num1 > num2 && num1 > num3) {
+  console.log(`The largest number out of the three numbers is ${num1}`);
+} else if (num2 > num1 && num2 > num3) {
+  console.log(`The largest number out of the three numbers is ${num2}`);
+} else {
+  console.log(`The largest number out of the three numbers is ${num3}`);
+}
